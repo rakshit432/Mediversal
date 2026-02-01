8dcf9c21b41ed672400a272f6452161e56e85eb1
Rakshit <rakshitkumar.5905@gmail.com>
2026-02-01 11:57:06 +0530

    removed doctor-list from doctor sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -18,8 +18,7 @@ const Sidebar = () => {
           <NavLink
             to="/admin-dashboard"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
               }`
             }
           >
@@ -30,8 +29,7 @@ const Sidebar = () => {
           <NavLink
             to="/all-appointments"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
               }`
             }
           >
@@ -42,8 +40,7 @@ const Sidebar = () => {
           <NavLink
             to="/add-doctor"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
               }`
             }
           >
@@ -54,8 +51,7 @@ const Sidebar = () => {
           <NavLink
             to="/doctor-list"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
               }`
             }
           >
@@ -67,14 +63,13 @@ const Sidebar = () => {
       )}
 
       {/* ================= DOCTOR SIDEBAR ================= */}
-      {!!dToken  && (
+      {!!dToken && (
         <ul className="text-[#515151] mt-5">
 
           <NavLink
             to="/doctor-dashboard"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
               }`
             }
           >
@@ -85,8 +80,7 @@ const Sidebar = () => {
           <NavLink
             to="/doctor-appointments"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
               }`
             }
           >
@@ -97,8 +91,7 @@ const Sidebar = () => {
           <NavLink
             to="/doctor-profile"
             className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
+              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-teal-50 border-r-4 border-primary' : ''
               }`
             }
           >
@@ -106,17 +99,7 @@ const Sidebar = () => {
             <p className="hidden md:block">Profile</p>
           </NavLink>
 
-          <NavLink
-            to="/doctor-list"
-            className={({ isActive }) =>
-              `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${
-                isActive ? 'bg-[#F2F3FF] border-r-4 border-primary' : ''
-              }`
-            }
-          >
-            <img src={assets.people_icon} alt="" />
-            <p>Doctors List</p>
-          </NavLink>
+
 
         </ul>
       )}
